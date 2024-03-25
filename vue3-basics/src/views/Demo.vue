59af2a984aa593de3eed5edb88bf12eeced90523
wangxinzhan <1727276432@qq.com>
2024-03-25 21:46:49 +0800

    刷提交

---
+++ vue3-basics/src/views/Demo.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import { ref } from "vue";
+
+  const index = ref(0);
+
+  const indexA = () => {
+    console.log("？");
+  }
+</script>
+
+<template>
+  <div>
+     
+  </div>
+</template>
+
+<style lang="css">
+    
+</style>
